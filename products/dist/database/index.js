e3bfd6388d3e05556613b03bbd2d24c26d04078e
VasuDevrani <vasu2002devrani@gmail.com>
2023-05-03 11:45:37 +0530

    cleanup

--- products/dist/database/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const product_repo_1 = __importDefault(require("./repository/product.repo"));
-exports.default = { ProductRepository: product_repo_1.default };
--- shopping/dist/database/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const connection_1 = __importDefault(require("./connection"));
-const shopping_repositoy_1 = __importDefault(require("./repository/shopping.repositoy"));
-exports.default = { databaseConnection: connection_1.default, ShoppingRespository: shopping_repositoy_1.default };
